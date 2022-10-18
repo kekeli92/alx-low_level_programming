a9a654f1dfc4e4ef77119f3d3f9195622a0468b7
kekeli92 <lytqua@gmail.com>
2022-10-18 11:46:24 -0700

    I somtimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - void function used to print lowercase alphabets
+ *
+ * Always 0
+ */
+void print_alphabet(void)
+{
+	char wrd;
+
+	for (wrd = 'a'; wrd <= 'z'; wrd++)
+		_putchar(wrd);
+
+	_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-A program that prints _putchar
+This fucntion prints alphabet
