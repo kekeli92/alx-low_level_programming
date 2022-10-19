8a849c74067a84d9f2a64a75cbdf0bfa6161febc
kekeli92 <lytqua@gmail.com>
2022-10-19 13:25:23 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - function that prints 10 times the alphabet
+ *print_alphabet_x10 - function that prints 10 times the alphabet
  *
  */
 void print_alphabet_x10(void)
