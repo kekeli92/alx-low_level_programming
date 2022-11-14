09a4fa17022ec7e7ac79625eaa949d1202a0981e
kekeli92 <lytqua@gmail.com>
2022-11-14 09:39:26 -0800

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
  
 /* _3_FUNCTION_LIKE_MACRO */
 #endif
