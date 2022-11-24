c6a3836add4b3b8a9e24be27e57c6c472552a8ee
kekeli92 <lytqua@gmail.com>
2022-11-24 15:16:05 -0800

    64-bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+/**64-bit program in assembly that prints Hello, Holberton */
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
